652e789c3c23a8e23a34a106efb30910b8c32fc0
fatemeh.Asgari <f.asgari2001@gmail.com>
2020-05-19 02:48:43 +0430

    Test for createAccount method

---
+++ src/test/java/controllerTest/GeneralControllerTest.java
@@ -0,0 +1,51 @@
+package controllerTest;
+
+import Main.controller.GeneralController;
+import Main.model.accounts.Account;
+import Main.model.accounts.BuyerAccount;
+import Main.model.accounts.ManagerAccount;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class GeneralControllerTest {
+    GeneralController generalController = new GeneralController();
+
+    @Test
+    public void createAccountTest1() {
+        String type = "buyer";
+        String username = "username";
+        BuyerAccount buyerAccount = new BuyerAccount("username", "firstName",
+                "lastName", "email@email.email", "09101111111", "password", 5000.5);
+        BuyerAccount.addBuyer(buyerAccount);
+        Assert.assertEquals("this userName is already taken.", generalController.createAccount(type, username));
+    }
+
+    @Test
+    public void createAccountTest2() {
+        String type = "manager";
+        ManagerAccount managerAccount = new ManagerAccount("username", "firstName",
+                "lastName", "email@email.email", "09101111111", "password");
+        ManagerAccount.addManager(managerAccount);
+        Assert.assertEquals("you can not create more than one manager account directly.",
+                generalController.createAccount(type,"user"));
+    }
+    @Test
+    public void createAccountTest3(){
+        String type = "seller";
+        Assert.assertEquals("your creating account request was sent to manager successfully.",
+                generalController.createAccount(type,"ttd"));
+    }
+    @Test
+    public void createAccountTest4() throws Exception {
+        Account.getReservedUserNames().add("user4");
+        Assert.assertEquals("this userName is already reserved.",
+                generalController.createAccount("buyer","user4"));
+
+    }
+    @Test
+    public void createAccountTest5(){
+        Assert.assertEquals("account Created.",generalController.createAccount("buyer","user5"));
+    }
+}
