53bf912952f0f8414a49dc42934c4905edfe30b8
fatemeh.Asgari <f.asgari2001@gmail.com>
2020-06-09 19:27:25 +0430

    Add some plugins and dependencies

--- src/main/java/Main/graphicView/GraphicMain.java
+++ src/main/java/Main/graphicView/GraphicMain.java
@@ -1,11 +1,21 @@
 package Main.graphicView;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class GraphicMain{
+public class GraphicMain extends Application{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
+
+
 }
