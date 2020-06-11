ed46e064e65ba2d952ba464b665eed8ace5656d1
Mahta Fetrat <77fetrat@gmail.com>
2020-06-11 13:58:47 +0430

    set primary methods to switch between scenes

--- src/main/java/Main/graphicView/GraphicMain.java
+++ src/main/java/Main/graphicView/GraphicMain.java
@@ -1,5 +1,6 @@
 package Main.graphicView;
 
+import Main.graphicView.scenes.MainMenu;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,23 +8,49 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
-public class GraphicMain extends Application{
+public class GraphicMain extends Application {
 
-    public static Stage stage;
+    private static ArrayList<Scene> sceneTrace = new ArrayList<>();
+    private static Stage stage;
+    public static GraphicMain graphicMain = new GraphicMain();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        Parent root = FXMLLoader.load(new File("src/main/java/Main/graphicView/scenes/mainMenu.fxml").toURI().toURL());
-        stage.setTitle("MFM SHOP");
-        stage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load(new File(MainMenu.FXML_PATH).toURI().toURL());
+        Scene scene = new Scene(root);
+        sceneTrace.add(scene);
+        stage.setTitle(MainMenu.TITLE);
+        stage.setScene(scene);
         primaryStage.show();
     }
 
+    public void back() {
+        if (sceneTrace.size() != 1) {
+            sceneTrace.remove(sceneTrace.size() - 1);
+            stage.setScene(sceneTrace.get(sceneTrace.size() - 1));
+        }
+    }
 
+    public void goToPage(String fxmlPath, String title) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(new File(fxmlPath).toURI().toURL());
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        sceneTrace.add(scene);
+        stage.setTitle(title);
+        stage.setScene(scene);
+    }
+
+    public Object getController(String fxmlName) throws MalformedURLException {
+        FXMLLoader fxmlLoader = new FXMLLoader(new File(fxmlName).toURI().toURL());
+        return fxmlLoader.getController();
+    }
 }
---
+++ src/main/java/Main/graphicView/scenes/ProductsPage.java
@@ -0,0 +1,10 @@
+package Main.graphicView.scenes;
+
+import Main.graphicView.GraphicMain;
+import javafx.scene.input.MouseEvent;
+
+public class ProductsPage {
+    public static final String FXML_PATH = "src/main/java/Main/graphicView/scenes/productsPage.fxml";
+    public static final String TITLE = "All Products";
+
+}
