c6d87744b14dc7c80fc1846aef1a58a4c203bb87
maryamsadat <razavi.mst@gmail.com>
2020-06-12 18:56:20 +0430

    add buyer and manager scene files

--- src/main/java/Main/graphicView/GraphicMain.java
+++ src/main/java/Main/graphicView/GraphicMain.java
@@ -17,11 +17,12 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GraphicMain extends Application {
 
     public static MediaPlayer buttonSound = new MediaPlayer(new Media(Paths.get("src/main/java/Main/graphicView/resources/soundEffects/buttonSound.wav").toUri().toString()));
-    public static ArrayList<Scene> sceneTrace = new ArrayList<>();
+    public static HashMap<Scene,String> sceneTrace = new HashMap<>();
     public static Stage stage;
     public static GraphicMain graphicMain = new GraphicMain();
 
@@ -40,7 +41,8 @@ public class GraphicMain extends Application {
         }
         stage.setTitle(LoginSignUpPage.TITLE);
         Scene scene = new Scene(root);
-        sceneTrace.add(scene);
+        sceneTrace.put(scene,LoginSignUpPage.TITLE);
+        stage.setTitle(LoginSignUpPage.TITLE);
         stage.setScene(scene);
         LoginSignUpPage.mediaPlayer.play();
         primaryStage.show();
@@ -48,11 +50,12 @@ public class GraphicMain extends Application {
 
     public void back() {
         if (sceneTrace.size() != 1) {
-            sceneTrace.remove(sceneTrace.size() - 1);
-            //TODO set back title
-            stage.setScene(sceneTrace.get(sceneTrace.size() - 1));
+            sceneTrace.remove(sceneTrace.keySet().toArray()[sceneTrace.size() - 1]);
+            stage.setTitle(sceneTrace.get(sceneTrace.keySet().toArray()[sceneTrace.size() - 1]));
+            System.out.println(sceneTrace.get(sceneTrace.keySet().toArray()[sceneTrace.size() - 1]));
+            stage.setScene(((Scene) sceneTrace.keySet().toArray()[sceneTrace.size() - 1]));
         } else {
-            System.exit(12345);
+            System.exit(123);
         }
     }
 
@@ -60,7 +63,7 @@ public class GraphicMain extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(new File(fxmlPath).toURI().toURL());
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
-        sceneTrace.add(scene);
+        sceneTrace.put(scene,title);
         GraphicMain.stage.setTitle(title);
         GraphicMain.stage.setScene(scene);
     }
